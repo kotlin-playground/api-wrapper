d60e7a88b727a2bfab9d37dbf767752cd251b580
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-11-07 20:24:42 +0700

    test aspect

--- src/main/kotlin/zircle/api/Settings.kt
+++ src/main/kotlin/zircle/api/Settings.kt
@@ -1,34 +1,46 @@
 package zircle.api
 
+import org.alfresco.cmis.client.impl.AlfrescoFolderImpl
 import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.Session
+import org.apache.chemistry.opencmis.client.runtime.PropertyImpl
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl
 import org.apache.chemistry.opencmis.commons.PropertyIds
 import org.apache.chemistry.opencmis.commons.SessionParameter
 import org.apache.chemistry.opencmis.commons.enums.BindingType
+import java.util.*
 
-object Constants {
+object QConstants {
     val CM_TITLE = "cm:title"
     val CM_DESCRIPTION = "cm:description"
-    var CM_FOLDER_WITH_TITLE = "cm:folder, P:cm:titled"
+    val CM_FOLDER_WITH_TITLE = "cm:folder, P:cm:titled"
+    val CMIS_OBJECT_ID = "cmis:objectId"
 }
 
-data class AppConfig(
+data class QAppConfig(
         val alfrescoUrl: String,
         val user: String,
         val password: String)
 
-data class RepositoryConfig(
+data class QRepositoryConfig(
         val atomPubUrl: String,
         val user: String,
         val password: String)
 
-data class FolderInfo(
+data class QFolderInfo(
     var name: String,
     var title: String,
     var description: String)
 
-class RepositoryApi(config: RepositoryConfig) {
+data class QProperty(
+        val name: String,
+        val value: Any)
+
+data class QAspect(
+        val name: String,
+        val properties:Array<QProperty>)
+
+class QRepositoryApi(config: QRepositoryConfig) {
 
     private var session: Session
 
@@ -44,20 +56,34 @@ class RepositoryApi(config: RepositoryConfig) {
             this.session = repository.createSession()
     }
 
+    fun removeAspectFromFolder(path: String, aspect: String): String {
+        var document = session.getObjectByPath(path) as AlfrescoFolderImpl
+        document.removeAspect(aspect)
+        var uuid = document.getProperty<String>(QConstants.CMIS_OBJECT_ID) as PropertyImpl
+        return uuid.getValue()
+    }
+
+    fun addAspectToFolder(path: String, aspect: QAspect) : Unit {
+        var document = session.getObjectByPath(path) as AlfrescoFolderImpl
+        var map = HashMap<String, Any>()
+        aspect.properties.forEach { map.put(it.name, it.value) }
+        document.addAspect(aspect.name, map)
+    }
+
     fun getFilder(path: String): Folder {
        var folder = session.getObjectByPath(path)
         return folder as Folder
     }
 
-    fun createFolder(path: String, info: FolderInfo): Folder {
+    fun createFolder(path: String, info: QFolderInfo): Folder {
         var baseFolder = path.substring(0, path.indexOf('/') + 1)
         var folderName = path.substring(path.lastIndexOf("/") + 1)
         var targetBaseFolder = session.getObjectByPath(baseFolder) as Folder
         var properties = hashMapOf(
-                PropertyIds.OBJECT_TYPE_ID to Constants.CM_FOLDER_WITH_TITLE,
+                PropertyIds.OBJECT_TYPE_ID to QConstants.CM_FOLDER_WITH_TITLE,
                 PropertyIds.NAME to folderName,
-                Constants.CM_TITLE to info.title,
-                Constants.CM_DESCRIPTION to info.description)
+                QConstants.CM_TITLE to info.title,
+                QConstants.CM_DESCRIPTION to info.description)
         var folder = targetBaseFolder.createFolder(properties)
         return folder
     }
--- src/test/kotlin/zircle/RepositoryApiSpec.kt
+++ src/test/kotlin/zircle/RepositoryApiSpec.kt
@@ -1,8 +1,10 @@
 package zircle
 
-import zircle.api.RepositoryApi
-import zircle.api.RepositoryConfig
+import zircle.api.QRepositoryApi
+import zircle.api.QRepositoryConfig
 import org.junit.Test
+import zircle.api.QAspect
+import zircle.api.QProperty
 
 object TestConfig {
     var atomPubUrl = "http://127.0.0.1:8081/alfresco/api/-default-/public/cmis/versions/1.0/atom"
@@ -12,14 +14,34 @@ object TestConfig {
 
 class RepositoryApiSpec {
 
-    @Test fun shoudGetFolderByPath() {
-        var path = "/"
-        var config = RepositoryConfig(
+    fun getApi(): QRepositoryApi {
+        var config = QRepositoryConfig(
                 user = TestConfig.user,
                 password = TestConfig.password,
                 atomPubUrl = TestConfig.atomPubUrl)
-        var api = RepositoryApi(config)
-        var folder = api.getFilder(path)
+        var api = QRepositoryApi(config)
+        return api
+    }
 
+    @Test fun shoudGetFolderByPath() {
+        var path = "/"
+        var api = this.getApi()
+        var folder = api.getFilder(path)
+    }
+
+    @Test fun shouldRemoveAspectFromFolder() {
+        var path = "/wk"
+        var api = this.getApi()
+        var uuid = api.removeAspectFromFolder(path, "P:cm:titled")
+    }
+
+    @Test fun shouldAddAspectToFolder() {
+        var path = "/wk"
+        var api = this.getApi()
+        var aspect = QAspect("P:cm:titled", arrayOf(
+                QProperty("cm:title", "Hello, world!"),
+                QProperty("cm:description", "What's your name?")
+        ))
+        api.addAspectToFolder(path, aspect)
     }
 }
